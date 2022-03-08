2bd758bfe4747d1f15ed8bce3ae8c7839bfec75a
Mhmd.S <mhmdslmn20@gmail.com>
2022-03-08 23:18:33 +0800

    message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import './style.css';
+import './css/col1Style.css';
+import './css/col2Style.css';
+import './css/col3Style.css';
+import './css/formStyle.css';
+import './css/mainStyle.css';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
